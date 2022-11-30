ccfc3130ddb45bf1a1e102bb1d252c74d7651abe
Khaled Guesmi <Khaled.Guesmi@mssolutions-group.com>
2022-11-30 11:15:27 +0100

    traduction of website

---
+++ js/db.js
@@ -0,0 +1,109 @@
+var data = {
+    "english":
+    {
+        "text_about_us": "text_about_us",
+        "desc_about_us":
+            "Orizzonti training & consulting objective is providing Italian language classes and Communication skills to students that need certification in Italian. Our center has an approved certificate in the Italian language by cert.it sis in Tunisia." +
+            "We provide as well courses in other languages with equal value to our students .",
+        //category Section
+        "category_header1": "Subjects",
+        "category_header2": "Explore Top Subjects",
+        "category_header3_it": "Italian",
+        "category_header3_en": "English",
+        "category_header3_ar": "Easy Arabic",
+        "category_header3_fr": "French",
+        "category_header3_rs": "Russian",
+        "category_header3_gr": "German",
+        //courses
+        "crs_header1": "Courses",
+        "crs_header2": "Our Popular Courses",
+        "crs_nb_student": "Students",
+        "crs_lang_it": "Italian",
+        "crs_lang_en": "English",
+        "crs_lang_ar": "Easy Arabic",
+        "crs_lang_fr": "French",
+        "crs_lang_rs": "Russian",
+        "crs_lang_gr": "German",
+    },
+    "italian":
+    {
+        "text_about_us": "Chi siamo",
+        "desc_about_us":
+            "Orizzonti training & consulting obiettivo è fornire lezioni di lingua italiana e abilità comunicative agli studenti che necessitano di certificazione in italiano." +
+            "\nIl nostro centro ha un certificato approvato in lingua italiana da cert.it sis in Tunisia.\n" +
+            "Forniamo anche corsi in altre lingue con pari valore ai nostri studenti.",
+        //category Section
+        "category_header1": "Soggetti",
+        "category_header2": "Esplora argomenti principali",
+        "category_header3_it": "Italiano",
+        "category_header3_en": "Inglese",
+        "category_header3_ar": "Arabo facile",
+        "category_header3_fr": "Francese",
+        "category_header3_rs": "Russo",
+        "category_header3_gr": "Tedesco",
+        //courses
+        "crs_header1": "Corsi",
+        "crs_header2": "I Nostri Corsi Popolari",
+        "crs_nb_student": "Studenti",
+        "crs_lang_it": "Italiano",
+        "crs_lang_en": "Inglese",
+        "crs_lang_ar": "Arabo facile",
+        "crs_lang_fr": "Francese",
+        "crs_lang_rs": "Russo",
+        "crs_lang_gr": "Tedesco",
+
+    },
+    "francais":
+    {
+        "text_about_us": "À propos de nous",
+        "desc_about_us":
+            "Orizzonti Training & Consulting a pour objectif de fournir des cours de langue italienne et des compétences en communication aux étudiants qui en ont besoin" +
+            "certification en italien. Notre centre dispose d'un certificat approuvé en langue italienne par cert.it sis en Tunisie." +
+            "Nous proposons également des cours dans d'autres langues de valeur égale pour nos étudiants.",
+        //category Section
+        "category_header1": "Sujets",
+        "category_header2": "Explorer les sujets les plus populaires",
+        "category_header3_it": "Italien",
+        "category_header3_en": "Anglais",
+        "category_header3_ar": "Arabe facile",
+        "category_header3_fr": "Français",
+        "category_header3_rs": "Russe",
+        "category_header3_gr": "Allemand",
+        //courses
+        "crs_header1": "Cours",
+        "crs_header2": "Nos cours populaires",
+        "crs_nb_student": "Étudiants",
+        "crs_lang_it": "Italien",
+        "crs_lang_en": "Anglais",
+        "crs_lang_ar": "Arabe facile",
+        "crs_lang_fr": "Français",
+        "crs_lang_rs": "Russe",
+        "crs_lang_gr": "Allemand",
+
+    },
+    "arabe":
+    {
+        "text_about_us": "معلومات عنا",
+        "desc_about_us":
+            "هدف أوريزونتي للتدريب والاستشارات هو توفير دروس في اللغة الإيطالية ومهارات الاتصال للطلاب الذين يحتاجون إلى شهادة باللغة الإيطالية. مركزنا حاصل على شهادة معتمدة في اللغة الإيطالية من قبل cert.it sis في تونس ، كما نقدم دورات بلغات أخرى ذات قيمة متساوية لطلابنا.",
+        //category Section
+        "category_header1": "المواضيع",
+        "category_header2": "استكشاف أهم المواضيع",
+        "category_header3_it": "الإيطالية",
+        "category_header3_en": "الإنجليزية",
+        "category_header3_ar": "العربية السهلة",
+        "category_header3_fr": "الفرنسية",
+        "category_header3_rs": "الروسية",
+        "category_header3_gr": "الألمانية",
+        //courses
+        "crs_header1": "الدورات",
+        "crs_header2": "الدورات الشعبية لدينا",
+        "crs_nb_student": "الطلاب",
+        "crs_lang_it":  "الإيطالية",
+        "crs_lang_en": "الإنجليزية",
+        "crs_lang_ar": "العربية السهلة",
+        "crs_lang_fr": "الفرنسية",
+        "crs_lang_rs": "الروسية",
+        "crs_lang_gr": "الألمانية",
+    }
+}
